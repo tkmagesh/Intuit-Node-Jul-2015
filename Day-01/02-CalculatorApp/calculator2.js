2d513db0012d47b4b2b52266447cde7ad8e988df
tkmagesh <tkmagesh77@gmail.com>
2015-07-24 12:52:36 +0530

    Calculator Factory

--- Day-01/02-CalculatorApp/calculator2.js
+++ Day-01/02-CalculatorApp/calculator2.js
@@ -1,21 +1,23 @@
-var result = 0;
-var calculator = {
+function getCalculator(){
+    var result = 0;
+    var calculator = {
 
-    add : function(x){
-        result += x;
-    },
-    subtract : function(x){
-        result -= x;
-    },
-    multiply : function(x){
-        result *= x;
-    },
-    divide : function(x){
-        result /= x;
-    },
-    getResult : function(){
-        return result;
-    }
-};
-
-module.exports = calculator;
+        add : function(x){
+            result += x;
+        },
+        subtract : function(x){
+            result -= x;
+        },
+        multiply : function(x){
+            result *= x;
+        },
+        divide : function(x){
+            result /= x;
+        },
+        getResult : function(){
+            return result;
+        }
+    };
+    return calculator;
+}
+module.exports = getCalculator;
--- Day-01/02-CalculatorApp/calculatorApp-2.js
+++ Day-01/02-CalculatorApp/calculatorApp-2.js
@@ -1,4 +1,6 @@
-var c1 = require('./calculator2.js');
+var getCalculator = require('./calculator2.js');
+
+var c1 = getCalculator();
 c1.add(100);
 c1.subtract(50);
 c1.divide(5);
@@ -6,6 +8,6 @@ c1.multiply(3);
 
 console.log("c1 result = ", c1.getResult());
 
-var c2 = require('./calculator2.js');
+var c2 = getCalculator();
 c2.add(1000);
 console.log("c2 result = ", c2.getResult()); //should be 1000
