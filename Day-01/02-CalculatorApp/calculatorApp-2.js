4408f6a5406b5ddcfa012b187c60c10125b91d5e
tkmagesh <tkmagesh77@gmail.com>
2015-07-24 12:37:19 +0530

    Multiple calculator problem

--- Day-01/02-CalculatorApp/calculatorApp-2.js
+++ Day-01/02-CalculatorApp/calculatorApp-2.js
@@ -1,7 +1,11 @@
-var calculator = require('./calculator.js');
-calculator.add(100);
-calculator.subtract(50);
-calculator.divide(5);
-calculator.mulitply(3);
+var c1 = require('./calculator2.js');
+c1.add(100);
+c1.subtract(50);
+c1.divide(5);
+c1.multiply(3);
 
-console.log(calculator.getResult());
+console.log("c1 result = ", c1.getResult());
+
+var c2 = require('./calculator2.js');
+c2.add(1000);
+console.log("c2 result = ", c2.getResult()); //should be 1000
