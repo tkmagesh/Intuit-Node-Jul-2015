2703fd5a7e92928528c14349c6be7a2f179aea21
tkmagesh <tkmagesh77@gmail.com>
2015-07-24 15:02:39 +0530

    Static Server

--- Day-01/03-printFile/print.js
+++ Day-01/03-printFile/print.js
@@ -2,13 +2,29 @@ var fs = require("fs");
 
 //var fileContents = fs.readFileSync("test.txt", {encoding : 'utf8'});
 
-fs.readFile("test.txt", {encoding : 'utf8'}, function(err, fileContents){
+/*fs.readFile("test.txt", {encoding : 'utf8'}, function(err, fileContents){
     if (err){
         console.log(err);
         return;
     }
     console.log(fileContents);
-})
+})*/
 
+var stream = fs.createReadStream('test.txt', {encoding : 'utf8'});
+var readCount = 0;
+stream.pipe(process.stdout);
+
+stream.on('data', function(chunk){
+    ++readCount;
+  //console.log(chunk);
+});
+
+stream.on('end', function(){
+    console.log("Job done with " + readCount + " reads");
+});
+
+stream.on('error', function(err){
+    console.log("something went wrong..", err);
+});
 
 
---
+++ Day-01/06-WebServer/server.js
@@ -0,0 +1,15 @@
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+
+var server = http.createServer(function(req, res){
+    var resource = path.join(__dirname, req.url);
+    console.log(resource);
+    if (!fs.existsSync(resource)){
+        res.statusCode = 404;
+        res.end();
+        return;
+    }
+    fs.createReadStream(resource).pipe(res);
+});
+server.listen(9090);
