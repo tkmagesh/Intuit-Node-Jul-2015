0e65ebefd13b838d2382841d66608958877625d1
tkmagesh <tkmagesh77@gmail.com>
2015-07-24 12:01:18 +0530

    CalculatorApp - Part - 1

---
+++ Day-01/02-CalculatorApp/calculatorApp.js
@@ -0,0 +1,8 @@
+var calculator = require("./calculator.js")
+var number1 = 100,
+    number2 = 200;
+
+console.log(calculator.add(number1, number2));
+console.log(calculator.subtract(number1, number2));
+console.log(calculator.multiply(number1, number2));
+console.log(calculator.divide(number1, number2));
