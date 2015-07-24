17fb1beda2a50dfed60c2b183eed028b5020c225
tkmagesh <tkmagesh77@gmail.com>
2015-07-24 14:17:25 +0530

    batchCalculator

--- Day-01/04-async/async.js
+++ Day-01/04-async/async.js
@@ -17,19 +17,23 @@ function addClient(x,y){
 //Async (with bug)
 function addAsync(x,y, onResult){
    setTimeout(function(){
-      if (x === 0 || y === 0) throw new Error("Invalid arguments");
+      if (x === 0 || y === 0) {
+          var err =  new Error("Invalid arguments");
+          onResult(err, null)
+      }
+
       var result = x + y;
-      onResult(result);
+      onResult(null, result);
    },4000);
 }
 
 function addAsyncClient(x,y){
-   try{
-      add(x,y, function(result){
-          console.log("result = ", result);
-      });
-   } catch (e){
-       console.log("Something went wrong", e);
-   }
+  addAsync(x,y, function(err, result){
+      if (err){
+          console.log("Something went wrong", err);
+          return;
+      }
+      console.log("result = ", result);
+  });
 }
 
