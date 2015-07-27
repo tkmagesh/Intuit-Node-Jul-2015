c7555be9886271ec941df4d3277b47eff0c3944d
tkmagesh <tkmagesh77@gmail.com>
2015-07-27 10:35:05 +0530

    Async Solution

--- Async-Problem.js
+++ Async-Problem.js
@@ -1,38 +1,45 @@
-function f1(){
+function f1(next){
     console.log("f1 is triggered");
     setTimeout(function(){
         console.log("f1 is completed");
+        next();
     },3000)
 }
 
-function f2(){
+function f2(next){
     console.log("f2 is triggered");
     setTimeout(function(){
         console.log("f2 is completed");
+        next();
     },3000)
 }
 
-function f3(){
+function f3(next){
     console.log("f3 is triggered");
     setTimeout(function(){
         console.log("f3 is completed");
+        next();
     },3000)
 }
 
-function f4(){
+function f4(next){
     console.log("f4 is triggered");
     setTimeout(function(){
         console.log("f4 is completed");
+        next();
     },3000)
 }
 
 
-var fns = [f1, f2, f3, f4];
+var fns = [f4, f3, f2, f1];
 
 function exec(fns){
-    for(var i=0; i<fns.length; i++){
-        var fn = fns[i];
-        fn();
+    var first = fns[0];
+    var remaining = fns.splice(1)
+    var next = function(){
+        exec(remaining);
     }
+    if (typeof first === "function")
+        first(next);
 }
 
